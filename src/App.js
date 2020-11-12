c6dc055305b5f610239cca83e2fa7818f7aa9638
dudamaciej88 <dudamaciej88@gmail.com>
2020-11-12 21:05:09 +0100

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
-    <>
-    </>
+    <Router>
+      <Header />
+    </Router>
   );
-}
+};
 
 export default App;
