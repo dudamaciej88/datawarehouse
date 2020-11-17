c1de7ae22265e27c5b9991e1c59fe23994a6cc4e
dudamaciej88 <dudamaciej88@gmail.com>
2020-11-18 00:50:11 +0100

    Create component

---
+++ src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import About from '../components/About/About';
+
+const MainPage = () => {
+  return (
+    <>
+      <About />
+    </>
+  );
+};
+
+export default MainPage;
