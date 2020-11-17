7d3d85ae5175fd9a21b16d23fbdcf4a98c237c78
dudamaciej88 <dudamaciej88@gmail.com>
2020-11-18 00:46:57 +0100

    Add MainPage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header/Header';
+import MainPage from './pages/MainPage';
 
 const App = () => {
   return (
     <Router>
       <Header />
+      <MainPage/>
     </Router>
   );
 };
