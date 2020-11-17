06d03ea4d66117a9f0d12e35157c0d0ceee42aaf
dudamaciej88 <dudamaciej88@gmail.com>
2020-11-18 00:49:08 +0100

    Add max width 1200 and centered

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -4,9 +4,13 @@ export const HeaderContainer = styled.header`
   position: fixed;
   top: 0px;
   left: 0px;
+  right: 0px;
+  margin: 0px auto;
   display: flex;
   align-items: center;
+  max-width: 1200px;
   height: 60px;
   width: 100%;
   background-color: transparent;
+  z-index: 100;
 `;
