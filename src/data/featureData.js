0c358f2cabd841591d4b5bc6df004b5b08eeb629
dudamaciej88 <dudamaciej88@gmail.com>
2020-11-25 23:44:09 +0100

    Create array

---
+++ src/data/featureData.js
@@ -0,0 +1,45 @@
+import Image1 from '../assets/features/image1.png';
+import Image2 from '../assets/features/image2.png';
+import Image3 from '../assets/features/image3.png';
+import Image4 from '../assets/features/image4.png';
+import Rectangle1 from '../assets/features/rectangle1.png';
+import Rectangle2 from '../assets/features/rectangle2.png';
+import Rectangle3 from '../assets/features/rectangle3.png';
+import Rectangle4 from '../assets/features/rectangle4.png';
+
+const featureData = [
+  {
+    id: 1,
+    name: 'Search Data',
+    description:
+      'Don’t worry if your data is very large, the Data Warehoue provides a search engine, which is useful for making it easier to find data effectively saving time.',
+    backgroundImg: Rectangle1,
+    image: Image1,
+  },
+  {
+    id: 2,
+    name: '24 Hours Access',
+    description:
+      'Access is given 24 hours a full morning to night and meet again in the morning giving you comfort when you need data when urgent.',
+    backgroundImg: Rectangle2,
+    image: Image2,
+  },
+  {
+    id: 3,
+    name: 'Print Out',
+    description:
+      'Print out service gives you convenience if someday you need print data, just edit it all and just print it.',
+    backgroundImg: Rectangle3,
+    image: Image3,
+  },
+  {
+    id: 4,
+    name: 'Security Code',
+    description:
+      'Data Security is one of our best facilities. Allows for your files to be safer. The file can be secured with a code or password that you created, so only you can open the file.',
+    backgroundImg: Rectangle4,
+    image: Image4,
+  },
+];
+
+export default featureData;
