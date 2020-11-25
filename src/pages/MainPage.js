7b40ab077b081ea9141808334ec5027067d4733b
dudamaciej88 <dudamaciej88@gmail.com>
2020-11-25 23:44:23 +0100

    Add Features component

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import About from '../components/About/About';
+import Features from '../components/Features/Features';
 
 const MainPage = () => {
   return (
     <>
       <About />
+      <Features />
     </>
   );
 };
