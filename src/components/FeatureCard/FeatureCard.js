b8f5b7111d44114d28b29869c7a8a8d437c987ec
dudamaciej88 <dudamaciej88@gmail.com>
2020-11-26 23:27:23 +0100

    Create component

--- src/components/FeatureCard/FeatureCard.js
+++ src/components/FeatureCard/FeatureCard.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useAnimation } from 'framer-motion';
 import { Container, Container2, Img, H3, P } from './FeatureCard.style';
-import Btn from '../Btn/Btn';
+
 
 const FeatureCard = ({ backgroundImg, image, name, description }) => {
+  const ref = useRef(null);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    let observer = new IntersectionObserver(
+      (entries) =>
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            controls.start('visible');
+          }
+        }),
+      { threshold: 0.1 }
+    );
+    observer.observe(ref.current);
+
+    return () => (observer = observer && observer.disconnect());
+  });
   return (
-    <Container backgroundImg={backgroundImg}>
-      <Img src={image} alt=''></Img>
-      <Container2>
+    <Container
+      backgroundImg={backgroundImg}
+      ref={ref}
+      animate={controls}
+      initial='hidden'
+      variants={{
+        visible: { scale: 1 },
+        hidden: { scale: 0 },
+      }}
+      transition={{ ease: 'easeOut', duration: 1, delayChildren: 1 }}
+    >
+      <Img
+        src={image}
+        alt=''
+        initial='hidden'
+        variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
+        transition={{ ease: 'easeOut', duration: 1.5 }}
+      ></Img>
+      <Container2
+        initial='hidden'
+        variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
+        transition={{ ease: 'easeOut', duration: 1 }}
+      >
         <H3>{name}</H3>
         <P>{description}</P>
-        <Btn animate={{ opacity: 1 }}/>
+        
       </Container2>
     </Container>
   );
