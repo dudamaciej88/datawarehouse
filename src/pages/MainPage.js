8a488423200d0e3b277f2b8c9f426ee390008a5f
dudamaciej88 <dudamaciej88@gmail.com>
2020-11-26 23:28:27 +0100

    Add Testimonials component

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import About from '../components/About/About';
 import Features from '../components/Features/Features';
+import Testimonials from '../components/Testimonials/Testimonials';
 
 const MainPage = () => {
   return (
     <>
       <About />
       <Features />
+      <Testimonials />
     </>
   );
 };
