90f2d0da696632ff288d1e80a1b6bac40f3bfb7b
dudamaciej88 <dudamaciej88@gmail.com>
2020-11-26 23:28:08 +0100

    Create array

---
+++ src/data/testimonialsData.js
@@ -0,0 +1,28 @@
+const testimonialsdata = [
+  {
+    name: 'John Fang',
+    company: 'wordlawng.com',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore impedit perferendis deleniti magnam tempora animi eligendi nam! Quam, perspiciatis ad! Id mollitia excepturi veritatis enim illum possimus blanditiis quos voluptatibus',
+  },
+  {
+    name: 'Jeny Jeny',
+    company: 'UX Engineer',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore impedit perferendis deleniti magnam tempora animi eligendi nam! Quam, perspiciatis ad! Id mollitia excepturi veritatis enim illum possimus blanditiis quos voluptatibus',
+  },
+  {
+    name: 'Chuck Morris',
+    company: 'Developer',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore impedit perferendis deleniti magnam tempora animi eligendi nam! Quam, perspiciatis ad! Id mollitia excepturi veritatis enim illum possimus blanditiis quos voluptatibus',
+  },
+  {
+    name: 'Mia Ching',
+    company: 'Photograph',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore impedit perferendis deleniti magnam tempora animi eligendi nam! Quam, perspiciatis ad! Id mollitia excepturi veritatis enim illum possimus blanditiis quos voluptatibus',
+  },
+];
+
+export default testimonialsdata;
